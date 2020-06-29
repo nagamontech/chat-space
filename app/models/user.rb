ecbf020f22893259d1cbce37edb2877833f2e211
nagamontech <67036025+nagamontech@users.noreply.github.com>
2020-06-29 14:35:09 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+<<<<<<< Updated upstream
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :messages
+=======
+  validates :name, presence: true,uniqueness: true
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+<<<<<<< Updated upstream
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
 end
+=======
+  root "messages#index"
+end
+>>>>>>> Stashed changes
